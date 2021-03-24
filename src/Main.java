bfa6d613921af68fea088cb48e2657004c9eabcb
danielrosenthal2 <rosedb19@wfu.edu>
2021-03-24 17:34:25 -0400

    commit 3, first half plus comments done

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,40 @@ import java.util.Stack;
 import java.util.*;
 
 public class Main {
+    //requirement 2
     public static int evaluatePostfix(String expr) {
+        //requirement 3
+        //intializing integers and operands stack
         int result = 0;
         int num = 0;
         int num2 = 0;
         Stack<Integer> operands = new Stack<Integer>();
+        //while loop that works thru length of expr
         while (expr.length() > 0) {
            String c;
-
+            //checks to see if at end of expression or not, sets current string to either operand or operator
            if (expr.contains(" ")) {
                c = expr.substring(0, expr.indexOf(" "));
            }
+           //this else happens when at end of expression, basically a default expression
+           //sets expression length to 0 and exits while loop
            else {
                c = expr;
                expr = "";
            }
-            //System.out.println(c);
+           //checks if c is plus operator first
+            //pops the 2 stored integers and does operation
+            //pushes result back into stack
+            //reduces expr to next character so it can move on
             if (c.equals("+")) {
                 int operand1 = operands.pop();
                 int operand2 = operands.pop();
-                //System.out.println("test");
                 result = operand1 + operand2;
-                //System.out.println(result);
                 operands.push(result);
                 expr = expr.substring(expr.indexOf(" ") + 1);
+            //same thing as plus operator but with multiplication
             } else if (c.equals( "*")) {
                 for(int n: operands){
-                    //System.out.println("list " + n);
                 }
                 int operand1 = operands.pop();
                 int operand2 = operands.pop();
@@ -39,28 +46,14 @@ public class Main {
                 operands.push(result);
                 expr = expr.substring(expr.indexOf(" ") + 1);
             }
+            //this is where the operands are pushed into stack, also reduces what is left of expr
             else{
                num = Integer.parseInt(c);
                operands.push(num);
-            //System.out.println(num);
                expr = expr.substring(expr.indexOf(" ") + 1);
-                //System.out.println(expr);
                 }
-//               num2 = Integer.parseInt(c);
-//            System.out.println(num2);
-//               operands.push(num2);
-//            System.out.println("test1");
-
-
-
-
-
-
-            //System.out.println(num);
-
-
         }
-
+        //returns final result
         return result;
     }
 
@@ -68,6 +61,7 @@ public class Main {
 
         //requirement 1 - reading expressions file
         FileInputStream fis = null;
+        //try and catch for file not found, initializes fis
         try {
             fis = new FileInputStream("src/Postfixexperssions");
         } catch (FileNotFoundException e){
@@ -78,7 +72,7 @@ public class Main {
         while (reader.hasNextLine()) {
 
             String lines = reader.nextLine();
-            //test
+            //print line for reference
             System.out.println(lines);
             //send to evaluate
             System.out.println("result " + evaluatePostfix(lines)) ;
